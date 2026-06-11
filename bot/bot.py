8ab72c6d86a82da9f9f5e44f6594566c1c68bf06
Charles Wickham <charles.wickham.consulting@gmail.com>
2026-06-11 17:40:11 +0200

    fix(bot): v12.7.7

--- bot/bot.py
+++ bot/bot.py
@@ -26,7 +26,7 @@ from sc2.unit import Unit
 from sc2.units import Units
 from .utils.unit_tags import zerg_townhalls, creep
 
-VERSION: str = "12.7.6"
+VERSION: str = "12.7.7"
 
 class WickedBot(Superbot):
     NAME: str = "WickedBot"
